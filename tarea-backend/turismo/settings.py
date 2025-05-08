ad1a7dca919df59f3a1b56bc957f601e32537136
MrBlair30 <blairdelgado41@gmail.com>
2025-05-07 21:19:30 -0500

    b

--- tarea-backend/turismo/settings.py
+++ tarea-backend/turismo/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-4(&^!frrvdhztt8sln)%$v%f*it02y8jy&5u)^jwzmpv1jho&2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
